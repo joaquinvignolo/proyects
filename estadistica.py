d9df4207e5f32b66058c7319befd81193e49b2e5
Kressinho <joacovignolo@gmail.com>
2024-05-15 20:55:26 -0300

    updated

--- estadistica.py
+++ estadistica.py
@@ -1,9 +1,7 @@
 import statistics
 
 def calcular_media(lista):
-    suma = sum(lista)
-    media = suma / len(lista)
-    return media
+    return statistics.mean(lista)
 
 def calcular_moda(lista):
     return statistics.mode(lista)
@@ -20,8 +18,11 @@ def calcular_varianza(lista):
     return varianza
 
 def menu():
+    
     opcion = None
+    
     try:
+        
         print("\n")
         print("0. Para cerrar el menú.")
         print("1. Visualizar la MEDIA.")
@@ -29,18 +30,20 @@ def menu():
         print("3. Visualizar la MEDIANA.")
         print("4. Visualizar la DESVIACIÓN ESTANDAR.")
         print("5. Visualizar la VARIANZA.\n")
-        opcion = int(input("Ingrese la opción que desea visualizar: "))
-    except ValueError:
+        
+        opcion = int(input("Ingrese el número de la opción que desea visualizar: "))
+    
+    except ValueError:    
         print("Por favor, ingrese un número válido.")
     return opcion
 
 def ingresar_datos():
     cantidad = 0
     numeros = []
-    print('Cuando termine de ingresar los datos escriba "CERRAR" para abrir el menú \n')
+    print('Cuando termine de ingresar los datos escriba "n" para abrir el menú \n')
     while True:
         numero = input(f"Ingrese el dato número {cantidad + 1}: ")
-        if numero.upper() == "CERRAR":
+        if numero.lower() == "n":
             break
         else:
             try:
@@ -48,7 +51,7 @@ def ingresar_datos():
                 cantidad += 1
                 numeros.append(numero)
             except ValueError:
-                print("Por favor, ingrese un número válido o escriba 'CERRAR' para terminar.")
+                print("Por favor, ingrese un número válido o escriba 'n' para terminar.")
     return numeros
 
 def main():
@@ -56,6 +59,7 @@ def main():
     opcion = menu()
     
     while opcion != 0:
+        
         if opcion == 1:
             print(f"La media es: {calcular_media(numeros)}")
         elif opcion == 2:
